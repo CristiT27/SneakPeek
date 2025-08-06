28b2b7feb3bbde6cddde3fd91b99c680fdc0f4ba
Cristian Trif <cristiantrif29@gmail.com>
2025-08-06 12:59:24 +0300

    Initial

---
+++ db/seed.ts
@@ -0,0 +1,14 @@
+import { PrismaClient } from "@prisma/client";
+import sampleData from "./sample-data";
+import css from "styled-jsx/css";
+
+async function main() {
+  const prisma = new PrismaClient();
+  await prisma.product.deleteMany();
+
+  await prisma.product.createMany({ data: sampleData.products });
+
+  console.log("Database seeded successfuly!");
+}
+
+main();
